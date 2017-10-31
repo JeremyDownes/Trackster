bf4cd61ab0867a388bdb04b968baee30da963b26
Jeremy Downes <JeremyDownes@hotmail.com>
2017-10-30 21:18:01 -0400

    first commit almost finished

---
+++ resources/js/trackster.js
@@ -0,0 +1,52 @@
+var Trackster = {};
+const API_KEY = "40f13d2f0721f3e75a883099fcdd8243";
+
+$(document).ready( function(){ 
+	$("input").click( function() {
+		$(this).attr("value","");
+	});
+
+	$(".glyphicon-remove").click( function() {
+		$("input").val("");
+	});
+
+	$("#submit").click( function(){
+		Trackster.searchTracksByTitle($("input[name='search']").val());
+	});
+})
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks. 
+*/
+Trackster.renderTracks = function(tracks) {
+	for (track in tracks) {
+		var song = '<div class="row results">'+
+        '<div class="col-xs-1 play"><a href="'+tracks[track].url+'" target="_blank"><i class="fa fa-play-circle-o"></i></a></div>'+
+        '<div class="col-xs-4"><label>'+(Number(track)+1)+'</label>'+tracks[track].name+'</div>'+ 
+        '<div class="col-xs-4">'+tracks[track].artist+'</div>'+
+        '<div class="col-xs-1"><img src="'+tracks[track].image[0]["#text"]+'"></div>'+
+        '<div class="col-xs-1">'+tracks[track].listeners+'</div>'+
+        '<div class="col-xs-1">3:35</div>'+
+      '</div>'+
+      '<div class="divider"></div>';
+
+		$("#results").append(song);
+	}
+};
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+Trackster.searchTracksByTitle = function(title) {
+	$.ajax({url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+API_KEY+"&format=json", success: function(result){
+		var tracks = result.results.trackmatches.track;
+    Trackster.renderTracks(tracks);
+    console.log(result);
+  }});
+
+};
+
+//AP_KEY = 40f13d2f0721f3e75a883099fcdd8243		"http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key="+API_KEY+"&format=json"
+ 
+ 	
