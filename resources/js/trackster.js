15c89d3a607d50753ec65a877aaeb415abefb797
Jeremy Downes <JeremyDownes@hotmail.com>
2017-10-30 22:04:56 -0400

    Events crippled after ajax

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -2,36 +2,49 @@ var Trackster = {};
 const API_KEY = "40f13d2f0721f3e75a883099fcdd8243";
 
 $(document).ready( function(){ 
-	$("input").click( function() {
-		$(this).attr("value","");
-	});
-
-	$(".glyphicon-remove").click( function() {
-		$("input").val("");
-	});
-
-	$("#submit").click( function(){
-		Trackster.searchTracksByTitle($("input[name='search']").val());
-	});
+  Trackster.onLoad();
 })
+
+Trackster.onLoad = function() {
+  $("input").click( function() {
+    $(this).attr("value","");
+  });
+
+  $(".glyphicon-remove").click( function() {
+    $("input").val("");
+  });
+
+  $("#submit").click( function(){
+    Trackster.searchTracksByTitle($("input[name='search']").val());
+  });
+
+  $("input").keyup( function(event) {
+      if (event.key === "Enter") {
+      Trackster.searchTracksByTitle($("input[name='search']").val());
+    }
+  });
+}
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks. 
 */
 Trackster.renderTracks = function(tracks) {
 	for (track in tracks) {
+    var listeners = tracks[track].listeners;
+    listeners = Number(listeners).toLocaleString();
 		var song = '<div class="row results">'+
         '<div class="col-xs-1 play"><a href="'+tracks[track].url+'" target="_blank"><i class="fa fa-play-circle-o"></i></a></div>'+
         '<div class="col-xs-4"><label>'+(Number(track)+1)+'</label>'+tracks[track].name+'</div>'+ 
         '<div class="col-xs-4">'+tracks[track].artist+'</div>'+
         '<div class="col-xs-1"><img src="'+tracks[track].image[0]["#text"]+'"></div>'+
-        '<div class="col-xs-1">'+tracks[track].listeners+'</div>'+
+        '<div class="col-xs-1">'+listeners+'</div>'+
         '<div class="col-xs-1">3:35</div>'+
       '</div>'+
       '<div class="divider"></div>';
 
 		$("#results").append(song);
 	}
+  Trackster.onLoad();
 };
 
 /*
@@ -42,7 +55,7 @@ Trackster.searchTracksByTitle = function(title) {
 	$.ajax({url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+API_KEY+"&format=json", success: function(result){
 		var tracks = result.results.trackmatches.track;
     Trackster.renderTracks(tracks);
-    console.log(result);
+    Trackster.onLoad();
   }});
 
 };
